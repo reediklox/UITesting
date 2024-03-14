d7f213fbcb8e6da9927da79bea6ba7821bde9eac
reediklox <deerikcosmos@mail.ru>
2024-03-14 10:38:58 +0300

    Gmail Done

---
+++ GoogleMail/testMain.py
@@ -0,0 +1,99 @@
+from JSON.jsonWork import JWork
+import json
+import time
+import threading
+from webDriver import Driver
+import pandas as pd
+
+
+PATH_JSON = 'GoogleMail/userdata.json'
+PATH_JSON_NEW = 'GoogleMail/newUserData.json'
+KEY_JSON = 'Gmail'
+PASSWORD = JWork.getData(KEY_JSON, PATH_JSON)['password']
+LOGIN = JWork.getData(KEY_JSON, PATH_JSON)["login"]
+NEW_PASSWORD = JWork.getData(KEY_JSON, PATH_JSON_NEW)['password']
+
+AUTH_PATH = [   
+                '/html/body/header/div/div/div/a[2]',
+                '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/c-wiz/div/div[3]/div/div/c-wiz/section/div/div/div/div/div/div/header/div[3]/div/div/a',
+                '//*[@id="identifierId"]', 
+                '//*[@id="password"]/div[1]/div/div[1]/input', 
+                '//*[@id="gb"]/div[2]/div[3]/div[1]/div[2]/div/a'
+            ]
+AUTH_DATA = [
+                LOGIN,
+                PASSWORD
+            ]
+
+TABLE = {'Email': [AUTH_DATA[0]], 'Password': [AUTH_DATA[1]], 'Name/Last Name': None, 'Birth Date': None, 'Reserve Email': None}
+
+def ChangePass():
+    with open('GoogleMail/userdata.json', 'r') as r:
+        data = json.load(r)
+        
+    data['Gmail']['password'] = NEW_PASSWORD
+    
+    with open('GoogleMail/userdata.json', 'w') as w:
+        json.dump(data, w)
+
+def RunThread(url, page_name):
+    if page_name == 'personal_info':
+        person_driver = Driver(url)
+        person_driver.Authorithation(AUTH_PATH, AUTH_DATA)
+        
+        person_driver.ChangeUserInfo([
+            '//*[@jslog="164890;metadata:W1tudWxsLG51bGwsWzEwMDkwXV1d; track:click"]',
+            '//button[@aria-label="Edit Name"]',
+            '//*[@id="i6"]',
+            '//*[@id="i11"]',
+            '//button[@class="UywwFc-LgbsSe UywwFc-LgbsSe-OWXEXe-dgl2Hf wMI9H"]'
+        ],
+        [
+            JWork.getData(KEY_JSON, PATH_JSON_NEW)['name'],
+            JWork.getData(KEY_JSON, PATH_JSON_NEW)['lastname']
+        ])
+        time.sleep(5)
+        person_driver.closeBrowser()
+    elif page_name == 'security':
+        sec_driver = Driver(url)
+        sec_driver.Authorithation(AUTH_PATH, AUTH_DATA)
+        sec_driver.ChangePassword([
+            '//*[@aria-label="Password"]',
+            '//*[@id="i5"]',
+            '//*[@id="i11"]',
+            '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/c-wiz/div/div[4]/form/div/div[2]/div/div/button'
+        ], 
+            NEW_PASSWORD)
+        ChangePass()
+        sec_driver.closeBrowser()
+
+
+main_driver = Driver('https://www.google.com/intl/ru/gmail/about/')
+main_driver.Authorithation(AUTH_PATH, AUTH_DATA, True)
+
+urls = ['https://myaccount.google.com/personal-info', 'https://myaccount.google.com/security']
+actions = ['personal_info', 'security']
+
+thread1 = threading.Thread(target=RunThread, args=(urls[0], actions[0], ))
+thread2 = threading.Thread(target=RunThread, args=(urls[1], actions[1], ))
+    
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+person_driver = Driver(urls[0])
+person_driver.Authorithation(AUTH_PATH, [LOGIN, NEW_PASSWORD])
+NAME, BIRTH, RESERVE = person_driver.GetUserInfo()
+person_driver.closeBrowser()
+
+TABLE['Password'] = [NEW_PASSWORD]
+TABLE['Name/Last Name'] = [NAME]
+TABLE['Birth Date'] = [BIRTH]
+TABLE['Reserve Email'] = [RESERVE]
+
+df = pd.DataFrame(TABLE)
+df.to_excel('GoogleMail/Gmail.xlsx')
+
+main_driver.closeBrowser()
---
+++ JSON/jsonWork.py
@@ -0,0 +1,44 @@
+import json
+
+class JsonWorkException(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+            
+    def __str__(self):
+        if 'class' in self.message:
+            return f'ClassInstanceException: {self.message}'
+        elif 'path' in self.message:
+            return f'InvalidPathException: {self.message}'
+        else:
+            return 'Something went wrong in jsonWork.py module'
+
+class CreateObjectError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args
+
+
+class JWork():
+    def __init__(self):
+        raise JsonWorkException('You can not create the instance of this class')
+     
+    @classmethod
+    def __pathValidate(cls, path:str):
+        if '.' in path and path.split('.')[1] == 'json':
+            return path
+        else:
+            raise JsonWorkException('Something wrong with the path. Check the format!')
+    
+    @classmethod
+    def getData(cls, key:str, path:str) -> str:
+        path = JWork.__pathValidate(path)
+            
+        with open(path, 'r') as j:
+            data = json.load(j)
+        
+        return data[key]
+        
+        
